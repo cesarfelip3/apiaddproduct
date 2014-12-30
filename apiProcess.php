80f05faf73d50e1fdc42d5bf5d2de0dd9623e3fb
Samuel Adam <samuel@agilizelabs.com>
2014-12-30 21:54:21 +0545

    debg

--- apiProcess.php
+++ apiProcess.php
@@ -15,9 +15,11 @@ if ($_POST) {
     $data_string = json_encode($data);
 
     $ch = curl_init($restUrl);
+    //curl_setopt($ch, CURLOPT_COOKIE, 'frontend=wrqwrjwqr');
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //curl_setopt($ch, CURLOPT_HEADER, TRUE);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/jsonp',
         'Content-Length: ' . strlen($data_string))
