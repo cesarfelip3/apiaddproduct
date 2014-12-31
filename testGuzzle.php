9b1583d690a60d31bb2fc72da309468a455f2fd1
Samuel Adam <samuel@agilizelabs.com>
2014-12-31 21:22:03 +0545

    using Guzzle

---
+++ testGuzzle.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once('vendor/autoload.php');
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+try {
+    $client = new Client();
+//$response = $client->post('http://127.0.0.1/Magento/ussa/index.php/restconnect/index/addProduct', ['body' => ['productsku' => 'product1', 'qty' => 1]]);
+
+
+    $request = $client->createRequest('POST', 'http://dev.ussa.org/ecommerce/magento/index.php/restconnect/index/addProduct');
+    $request->setHeader('Set-Cookie', FALSE);
+    $request->setHeader('Content-Type', 'application/json');
+
+// 'application/json; charset=utf8'
+
+    $postBody = $request->getBody();
+
+// $postBody is an instance of GuzzleHttp\Post\PostBodyInterface
+    $postBody->setField('productsku', 'product1');
+    $postBody->setField('qty', 1);
+
+//echo $postBody->getField('foo');
+// 'bar'
+//echo json_encode($postBody->getFields());
+// {"foo": "bar"}
+// Send the POST request
+    $response = $client->send($request);
+    header('Set-Cookie: '.$response->getHeader('set-cookie'));
+    header('Location: http://dev.ussa.org/ecommerce/magento/index.php/checkout/cart');
+    exit();
+}
+catch (RequestException $e) {
+    echo $e->getRequest();
+    if ($e->hasResponse()) {
+        echo $e->getResponse();
+    }
+}
