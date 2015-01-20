846104160d531c5e71707768603fce740e3fd660
Samuel Adam <samuel@agilizelabs.com>
2015-01-20 22:12:18 +0545

    remove header, fix

--- example/addProductToCart.php
+++ example/addProductToCart.php
@@ -6,7 +6,7 @@
    -d apipassword=apipassword \
    -d sku=product11 \
    -d qty=1 \
-   -d method= 'addProductToCart'*/
+   -d method=addProductToCart*/
 $restUrl = 'http://dev.ussa.org/ecommerce/magento/index.php/restconnect/index/ussa';
 
 $data = array(
@@ -24,10 +24,10 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/jsonp',
-    'Content-Length: ' . strlen($data_string))
-);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//    'Content-Type: application/jsonp',
+//    'Content-Length: ' . strlen($data_string))
+//);
 
 $result = curl_exec($ch);
 
--- example/removeProductFromCart.php
+++ example/removeProductFromCart.php
@@ -16,10 +16,10 @@ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($ch, CURLOPT_HEADER, TRUE);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/jsonp',
-    'Content-Length: ' . strlen($data_string))
-);
+//curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+//    'Content-Type: application/jsonp',
+//    'Content-Length: ' . strlen($data_string))
+//);
 
 $result = curl_exec($ch);
 curl_close($ch);
