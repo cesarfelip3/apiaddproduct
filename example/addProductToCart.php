5ec6d92256f5bd6ceb824bb1f06fa0584a3faf45
Samuel Adam <samuel@agilizelabs.com>
2015-01-20 22:27:47 +0545

    add product to cart with user

--- example/addProductToCart.php
+++ example/addProductToCart.php
@@ -8,7 +8,6 @@
    -d qty=1 \
    -d method=addProductToCart*/
 $restUrl = 'http://dev.ussa.org/ecommerce/magento/index.php/restconnect/index/ussa';
-$restUrl = 'http://127.0.0.1/Magento/ussa/index.php/restconnect/index/ussa';
 
 $data = array(
     'email' => 'itmyprofession@gmail.com',
